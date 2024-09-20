f1fb0d5a544419da3362200c24b7d1cc12804dda
ma <md.ali2001@hotmail.com>
2024-09-21 02:48:27 +0500

    .

---
+++ src/Abstract.java
@@ -0,0 +1,24 @@
+abstract  class demo {
+
+    abstract void start() ;
+};
+
+
+ class drama extends demo{
+
+     @Override
+     void start() {
+
+     }
+ };
+
+
+public class Abstract {
+    public static void main(String [] args)
+    {
+
+
+
+
+
+}}
