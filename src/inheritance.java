e134cb1438b2bc50e633143860d6849db1a4b28a
Muhammad Ali Masood <md.ali2001@hotmail.com>
2026-02-22 00:55:15 +0500

    Update inheritance.java

--- src/inheritance.java
+++ src/inheritance.java
@@ -34,16 +34,35 @@ class child extends father
     }
 }
 
+class child1 extends father
+{
+    private String name;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
+
 public class Main
 {
 	public static void main(String[] args) {
 		
 		child child1 = new child();
-		System.out.println(child1.getName());
+		System.out.println(child1.getName());  // null
 	//	child1.speak();
 		//System.out.println(child1.getName());
 		child1.setName("Ali");
-		System.out.println(child1.getName());
+		System.out.println(child1.getName());   // ali
+		
+		
+		child child2 = new child();
+		System.out.println(child2.getName());   // null   child2 is in different memory
 		
 	}
 	
