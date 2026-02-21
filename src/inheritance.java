139a235f82b82d7a1953cb8fc6beba006e877bb8
Muhammad Ali Masood <md.ali2001@hotmail.com>
2026-02-22 00:44:11 +0500

    Create inheritance.java

---
+++ src/inheritance.java
@@ -0,0 +1,50 @@
+ class grandfather
+{
+private String name="ABC";
+
+public void speak ()
+{
+    System.out.println("grandfather is speaking");
+}
+
+
+}
+
+class father extends grandfather                           
+{
+private String name="ABC2";
+// public void speak ()
+// {
+//     System.out.println("father is speaking");
+// }
+}
+
+class child extends father
+{
+    private String name;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		child child1 = new child();
+		System.out.println(child1.getName());
+	//	child1.speak();
+		//System.out.println(child1.getName());
+		child1.setName("Ali");
+		System.out.println(child1.getName());
+		
+	}
+	
+}
